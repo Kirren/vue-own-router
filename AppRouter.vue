99ebfc2e3c73db67e5574eb0bf89d110024a86f0
Helen <kirren@live.com>
2018-05-04 17:09:01 +0300

    Add AppRouter vue file

---
+++ AppRouter.vue
@@ -0,0 +1,25 @@
+<!-- Router.vue -->
+<template>
+    <component :is="routedComponent"></component>
+</template>
+
+<script>
+	import HomePage from "./HomePage";
+	import ArticlesPage from "./ArticlesPage";
+
+	const routes = {
+		"/": HomePage,
+		"/articles": ArticlesPage
+	};
+
+	export default {
+		data() {
+			return { current: window.location.pathname };
+		},
+		computed: {
+			routedComponent() {
+				return routes[this.current];
+			}
+		}
+	};
+</script>
